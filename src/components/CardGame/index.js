1a8e34789a5439aa1626db7c8a94a1c1775f8d16
m3nininho <mel.cruc14@gmail.com>
2022-11-07 21:24:30 -0300

    :tada: Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame() {
+  return `
+    <article class="card-game">
+      <img class="interrogation" src="./images/interrogation.svg" alt="interrogation">
+    </article>
+  `;
+}
+
+export default CardGame;
